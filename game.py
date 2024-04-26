2ccc32830b307f60c6a4ff0d5e22f43fb5f8969b
Jackson-Davey <159150218+Jackson-Davey@users.noreply.github.com>
2024-04-26 01:13:32 +0000

    started

--- game.py
+++ game.py
@@ -1,1 +1,14 @@
-yes
+import time
+print()
+print("30 years after the imposing geological catastrophe")
+print()
+time.sleep(3)
+print("The surface of the earth, still sheathed by a thick layer of ash and dust")
+print()
+time.sleep(4)
+print("The consequences of purposeful disregardance...")
+print()
+print()
+time.sleep(3.25)
+while True:
+    print("Sanctuary awaits! Would you like to start your adventure? (Y or N)")
